b2d890500671644d8522a6b60b5cc436002a0ca0
ajakacky <ajakacky@my.ccsu.edu>
2021-10-09 14:45:35 -0400

    added two seperate rules

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -2,24 +2,26 @@ resource aws_security_group ec2_security_group {
   name        = local.security_group_name
   description = "Allow traffic for the Minecraft Server"
   vpc_id      = var.vpc_id
-  ingress     = [
-    {
-      description      = "Minecraft"
-      from_port        = 25565
-      to_port          = 25565
-      protocol         = "tcp"
-      cidr_blocks      = var.admin_ips
-    },
-    {
-      description      = "ssh"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = var.member_ips
-    }
-  ]
 
   tags = var.tags
+}
+
+resource aws_security_group_rule minecraft {
+  type              = "ingress"
+  from_port         = 25565
+  to_port           = 25565
+  protocol          = "tcp"
+  cidr_blocks       = var.member_ips
+  security_group_id = aws_security_group.ec2_security_group.id
+}
+
+resource aws_security_group_rule ssh {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = var.admin_ips
+  security_group_id = aws_security_group.ec2_security_group.id
 }
 
 resource aws_instance instance {
