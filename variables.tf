8af27f880070e118f5bb3b97cebc2b32cbdc0224
ajakacky <ajakacky@my.ccsu.edu>
2021-10-09 14:53:33 -0400

    added region

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable member_ips { sensitive = true }
 variable admin_ips  { sensitive = true }
 variable subnet_id  {}
 variable vpc_id     {}
+variable region     {}
 
 locals {
     deploy = yamldecode(file("deploy.yml"))
