90b1b67b26832fe7c95bbbea19cd5d66157f1613
Austin Jakacky <ajakacky@my.ccsu.edu>
2022-03-13 13:09:15 -0400

    added new video

--- modules/ec2-instance/variables.tf
+++ modules/ec2-instance/variables.tf
@@ -12,15 +12,10 @@ variable name                       {}
 variable instance_type              {}
 
 locals {
-  shutdown              = split(":", var.instance_lc.shutdown)
-  startup               = split(":", var.instance_lc.startup)
-  cron_shutdown         = "cron(${tonumber(local.shutdown[0])} ${tonumber(local.shutdown[1])} * * ? *)"
-  cron_startup          = "cron(${tonumber(local.startup[0])} ${tonumber(local.startup[1])} * * ? *)"
   security_group_name   = "security-group-${var.name}"
   iam_role_name         = "iam-role-${var.name}"
   iam_policy_name       = "iam-policy-${var.name}"
   instance_profile_name = "instance-profile-${var.name}"
-  cloudwatch_rule_name  = "rule-ebs-${name}"
   amis                  = {
     amazon-linux-2       = "ami-02e136e904f3da870"
     deep-learning-ubuntu = "ami-0e3c68b57d50caf64"
