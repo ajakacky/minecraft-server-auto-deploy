2d46630e95d7435e87dfaffd7eb5f01c5f423e60
Austin Jakacky <ajakacky@my.ccsu.edu>
2024-07-11 19:17:33 -0400

    update terraform directory

--- modules/ec2-instance/output.tf
+++
@@ -1,2 +0,0 @@
-output public_ip { value = aws_instance.instance.public_ip } 
-output arn { value = aws_instance.instance.arn } 
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable member_ips {}
-variable admin_ips  {}
-variable subnet_id  {}
-variable vpc_id     {}
-variable region     {}
-
-locals {
-    deploy = yamldecode(file("deploy.yml"))
-}
